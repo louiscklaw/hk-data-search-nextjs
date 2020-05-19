114124eff5fb26dd1d37fcb75b0a73ee239aabbf
louiscklaw <louiscklaw@gmail.com>
2020-05-19 15:29:41 +0800

    update api detail page,

--- src/app/pages/[lang]/[api_title]/index.js
+++ src/app/pages/[lang]/[api_title]/index.js
@@ -1,25 +1,93 @@
 import React, { useContext } from 'react';
 import Link from 'next/link';
 
-import {LangContext} from '../../../context'
+import {LangContext, GlobalContext} from '../../../context'
 import {Footer} from '../../../components'
 
-
 function ApiTitleIndexPage({api_title}){
-  const {getCurrentLang} = useContext(LangContext);
+  const {getCurrentLang} = useContext(LangContext)
+  const {getApiManifestByApiTitle}  = useContext(GlobalContext)
+  const api_manifest = getApiManifestByApiTitle(api_title)
+
+  const resources_list = getApiManifestByApiTitle(api_title).result.resources
+
+  function clearDoubleQuote(in_json_href){
+    return in_json_href.replace(/"/g,'')
+  }
+
+  function getHTMLLinkFromJSON(in_href){
+    const diluted_link = clearDoubleQuote(in_href)
+    return <a href={diluted_link} target="_blank">{diluted_link}</a>
+  }
+
+  function handleNullValue(in_value){
+    if (in_value==null){
+      return "NULL"
+    }else{
+      return in_value
+    }
+  }
+
+  function handleHTMLOutput(in_value){
+    if (in_value==null){
+      return "NULL"
+    }else if(in_value.search('http') > -1){
+      return getHTMLLinkFromJSON(in_value)
+    }else{
+      return in_value
+    }
+  }
 
   return(
     <>
-      {api_title}
-      <Link href={ `/${getCurrentLang()}` } >
-        <a>Back</a>
-      </Link>
+      <section>
+        <div class="container">
+          <Link href={ `/${getCurrentLang()}` } >
+            <a>Back</a>
+          </Link>
+          <div>
+            {api_title}
+          </div>
+        </div>
+      </section>
 
-      Defails
+      {
+        resources_list.map((x) => {
+          return (
+            <section>
+              <div class="container">
+                Resources: {x.shared_id}
+                <table class="table" style={{width: "100%"}}>
+                  <tbody>
+                      {
+                        ['description','url','format','is_api','last_modified'].map((y) => {
+                          return(
+                            <tr>
+                              <th>{y}</th>
+                              <td>{handleHTMLOutput(x[y])}</td>
+                            </tr>
+                          )
+                        })
+                      }
 
-      <pre>
-        {JSON.stringify({test: 'testing'}, null, 1)}
-      </pre>
+                  </tbody>
+                </table>
+              </div>
+            </section>
+          )
+        })
+
+      }
+
+
+      <section>
+        <div class="container">
+          <pre>
+            {JSON.stringify( getApiManifestByApiTitle(api_title) , null, 1)}
+          </pre>
+
+        </div>
+      </section>
 
       <Footer></Footer>
     </>
